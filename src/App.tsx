53da093ef1edf833724de29bdb0d0237520da7ce
SK_ERIC <974264052@qq.com>
2021-03-10 21:22:09 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {ProjectListScreen} from "screens/project-list";
+
+function App() {
+  return (
+    <div className="App">
+     <ProjectListScreen/>
+    </div>
+  );
+}
+
+export default App;
