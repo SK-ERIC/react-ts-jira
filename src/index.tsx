210b6ebee3a904a8bacfdf11b675fd6cdd587a93
SK_ERIC <974264052@qq.com>
2021-05-08 22:00:35 +0800

    chore: 安装 devtool

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+loadDevTools(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
 
 // If you want to start measuring performance in your app, pass a function
