5a03166656196682ec801908fe2f8fd03962fcae
SK_ERIC <974264052@qq.com>
2021-05-09 21:56:53 +0800

    style:  用 Row 布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screen/project-list";
 
@@ -17,8 +18,8 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
           <h3>Logo</h3>
           <h3>项目</h3>
           <h3>用户</h3>
@@ -27,48 +28,26 @@ export const AuthenticatedApp = () => {
           <button onClick={logout}>登出</button>
         </HeaderRight>
       </Header>
-      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen />
       </Main>
-      <Aside>aside</Aside>
-      <Footer>footer</Footer>
     </Container>
   );
 };
 
+// temporal dead zone(暂时性死区)
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas: "header header header" "nav main aside" "footer footer footer";
+  grid-template-rows: 6rem 1fr;
   height: 100vh;
-  grid-gap: 10rem;
 `;
 
-// grid-area 用来给 gird 子元素起名字
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+// grid-area 用来给grid子元素起名字
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
 `;
-
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
-const Main = styled.main`
-  grid-area: main;
-`;
-const Nav = styled.nav`
-  grid-area: nav;
-`;
-const Aside = styled.aside`
-  grid-area: aside;
-`;
-const Footer = styled.footer`
-  grid-area: footer;
-`;
+const Main = styled.main``;
