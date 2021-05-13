4af781947b216814d35ec57dce52d49091340708
SK_ERIC <974264052@qq.com>
2021-05-13 21:36:24 +0800

    feat: 添加 id-select.tsx

---
+++ src/components/id-select.tsx
@@ -0,0 +1,42 @@
+import { Select } from "antd";
+import { Raw } from "unauthenticated-app";
+
+// 用React方法取出Select组件上的类型
+type SelectProps = React.ComponentProps<typeof Select>;
+
+interface IdSelectProps
+  extends Omit<SelectProps, "value" | "onChange" | "options"> {
+  value: Raw | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
+}
+
+/**
+ * value 可以传入多种类型的值
+ * onChange 只会回调 number | undefined 类型
+ * 当 isNaN(Number(value)) 为 true 的时候，代表选择默认类型
+ * 当选择默认类型的时候， onChange 会回调 undefined
+ * @param props
+ */
+export const IdSelect = (props: IdSelectProps) => {
+  const { value, onChange, defaultOptionName, options, ...restProps } = props;
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+      {...restProps}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((option) => (
+        <Select.Option key={option.id} value={option.id}>
+          {option.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
